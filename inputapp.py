77ba35b24d1a8a3586fb9a1015fe46a74230e96a
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-03-15 21:06:28 +0100

    Update inputapp.py

--- inputapp.py
+++ inputapp.py
@@ -15,3 +15,15 @@ df = pd.DataFrame(columns=['Agent','Agence de visite','Raison de visite','Date d
 
 nombredevisites = st.number_input('Nombres de visites: ')
 st.write('Visites éfféctuées: ', nombredevisites)
+
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ecoactu.ma%2Fbadreddine-belghiti-nomme-directeur-general-de-wafa-ima-assistance%2F&psig=AOvVaw3O-CrJRv_9DT8a2fYGGD-I&ust=1647461105452000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCNpYL1yPYCFQAAAAAdAAAAABAR");
+background-size: cover;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
