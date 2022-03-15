dbd4a9e56d0e67ef2fe6dbce0d877bf80ce56174
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-03-15 22:07:34 +0100

    Update inputapp.py

--- inputapp.py
+++ inputapp.py
@@ -33,15 +33,15 @@ st.write(df)
 def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    df.to_excel(writer, index=False, sheet_name='1')
     workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
+    worksheet = writer.sheets['1']
     format1 = workbook.add_format({'num_format': '0.00'}) 
     worksheet.set_column('A:A', None, format1)  
     writer.save()
     processed_data = output.getvalue()
     return processed_data
 df_xlsx = to_excel(df)
-st.download_button(label='📥 Download Current Result',
+st.download_button(label='📥 Télécharger',
                                 data=df_xlsx ,
-                                file_name= 'df_test.xlsx')
+                                file_name= 'visites.xlsx')
