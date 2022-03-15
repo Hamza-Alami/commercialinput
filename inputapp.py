69bddc2589637057d085db65abfff518e028f718
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-03-15 21:02:28 +0100

    Update inputapp.py

--- inputapp.py
+++ inputapp.py
@@ -7,11 +7,11 @@ import os
 st.set_page_config(layout='wide')
 st.title('Application input des Agents WIA')
 
-DF creation
+#DF creation
 df = pd.DataFrame(columns=['Agent','Agence de visite','Raison de visite','Date de visite'])
 
-INPUTS
-N of visits
+#INPUTS
+#N of visits
 
 nombredevisites = = st.number_input('Insert a number')
 st.write('The current number is ', nombredevisites)
