80f4c160e41fd0dee09596f70ceab52bbd8fd3f0
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-03-15 21:16:24 +0100

    Update inputapp.py

--- inputapp.py
+++ inputapp.py
@@ -15,3 +15,25 @@ df = pd.DataFrame(columns=['Agent','Agence de visite','Raison de visite','Date d
 
 ndv = st.slider('Combien de visites avez vous effectuer?', 0, 100, 1)
 st.write("I'm ", ndv, 'years old')
+
+listagent = []
+listagence = []
+listraison = []
+listdate = []
+
+
+inagent= st.text_input('Nom')
+listagent.append(inagent)
+listagentfull = listagent*ndv
+
+#while len(listagence) < ndv:
+    #inagence = input('Agence: ')
+    #listagence.append(inagence)
+
+#while len(listraison) < ndv:
+    #inraison = input('Raison de visite: ')
+    #listraison.append(inraison)
+
+#while len(listdate) < ndv:
+    #indate = input('Date de visite: ')
+    #listdate.append(indate)
