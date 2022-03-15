09f27ea3f6c9cffdbe3015a3eab6f3f33c28388c
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2022-03-15 20:24:41 +0100

    Update inputapp.py

--- inputapp.py
+++ inputapp.py
@@ -4,14 +4,14 @@ import numpy as np
 import datetime
 import os
 
-#st.set_page_config(layout='wide')
-#st.title('Application input des Agents WIA')
+st.set_page_config(layout='wide')
+st.title('Application input des Agents WIA')
 
-#DF creation
-#df = pd.DataFrame(columns=['Agent','Agence de visite','Raison de visite','Date de visite'])
+DF creation
+df = pd.DataFrame(columns=['Agent','Agence de visite','Raison de visite','Date de visite'])
 
-#INPUTS
-#N of visits
+INPUTS
+N of visits
 
-#nombredevisites = = st.number_input('Insert a number')
-#st.write('The current number is ', nombredevisites)
+nombredevisites = = st.number_input('Insert a number')
+st.write('The current number is ', nombredevisites)
